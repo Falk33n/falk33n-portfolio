98b558e1c42d9f17379136da01f7ac19dc2cefb4
Falk33n <tim.falk00@gmail.com>
2024-12-01 17:56:09 +0100

    feat: add Toaster to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
+	import { Toaster } from '$components/feedback';
 	import '$src/app.css';
+	import { ContactForm } from '$src/routes/_components';
 
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div
+	class="min-h-screen w-full md:mx-auto md:w-[80vw] xl:w-[75vw] 2xl:w-[70vw]"
+>
+	<Toaster />
+
+	<main
+		id="content"
+		class="p-4"
+	>
+		{@render children()}
+
+		<ContactForm />
+	</main>
+</div>
