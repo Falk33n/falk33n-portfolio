2451506ee46b592cb4a418ef20f44c227f9ebfa3
Falk33n <tim.falk00@gmail.com>
2024-12-01 18:45:34 +0100

    feat: add modetoggle component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { Toaster } from '$components/feedback';
+	import { ModeToggle } from '$components/themes';
 	import '$src/app.css';
 	import { ContactForm } from '$src/routes/_components';
+	import { ModeWatcher } from 'mode-watcher';
 
 	let { children } = $props();
 </script>
@@ -9,12 +11,15 @@
 <div
 	class="min-h-screen w-full md:mx-auto md:w-[80vw] xl:w-[75vw] 2xl:w-[70vw]"
 >
+	<ModeWatcher />
 	<Toaster />
 
 	<main
 		id="content"
 		class="p-4"
 	>
+		<ModeToggle />
+
 		{@render children()}
 
 		<ContactForm />
