239d1c3d8c26230ad4064cc9fcf32c72e8243e99
Falk33n <tim.falk00@gmail.com>
2024-12-01 18:44:17 +0100

    feat: add tooltip

--- src/lib/components/feedback/index.ts
+++ src/lib/components/feedback/index.ts
@@ -1,3 +1,9 @@
+import { Tooltip as TooltipPrimitive } from 'bits-ui';
 import Toaster from './Toaster.svelte';
+import TooltipContent from './TooltipContent.svelte';
 
-export { Toaster };
+const Tooltip = TooltipPrimitive.Root;
+const TooltipTrigger = TooltipPrimitive.Trigger;
+const TooltipProvider = TooltipPrimitive.Provider;
+
+export { Toaster, Tooltip, TooltipContent, TooltipProvider, TooltipTrigger };
---
+++ src/lib/components/themes/ModeToggle.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import {
+		Tooltip,
+		TooltipContent,
+		TooltipProvider,
+		TooltipTrigger,
+	} from '$components/feedback';
+	import { Button } from '$components/forms';
+	import { Moon, Sun } from 'lucide-svelte/icons';
+	import { mode, toggleMode } from 'mode-watcher';
+
+	const Icon = $derived($mode === 'dark' ? Sun : Moon);
+	const title = $derived(
+		$mode === 'dark' ? 'Change to light mode' : 'Change to dark mode',
+	);
+</script>
+
+<TooltipProvider>
+	<Tooltip>
+		<TooltipTrigger onclick={toggleMode}>
+			{#snippet child({ props })}
+				<Button
+					variant="outline"
+					aria-live="polite"
+					aria-label={title}
+					size="icon"
+					{...props}
+				>
+					<Icon class="size-[1.2rem] transition-all dark:-rotate-90" />
+				</Button>
+			{/snippet}
+		</TooltipTrigger>
+		<TooltipContent>
+			{title}
+		</TooltipContent>
+	</Tooltip>
+</TooltipProvider>
