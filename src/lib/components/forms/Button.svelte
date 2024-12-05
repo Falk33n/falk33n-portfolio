c710e8bb740c8a4114bb2321f050c9cafee541a8
Falk33n <tim.falk00@gmail.com>
2024-12-05 17:53:48 +0100

    style: add full border-radius on icons

--- src/lib/components/forms/Button.svelte
+++ src/lib/components/forms/Button.svelte
@@ -27,7 +27,7 @@
 				default: 'h-10 px-4 py-2',
 				sm: 'h-9 rounded-md px-3',
 				lg: 'h-11 rounded-md px-8',
-				icon: 'h-10 w-10',
+				icon: 'h-10 w-10 rounded-full',
 			},
 		},
 		defaultVariants: {
@@ -54,6 +54,8 @@
 		size = 'default',
 		ref = $bindable(null),
 		href = undefined,
+		rel = undefined,
+		target = '_self',
 		type = 'button',
 		children,
 		...restProps
@@ -64,6 +66,8 @@
 	<a
 		bind:this={ref}
 		class={cn(buttonVariants({ variant, size, className }))}
+		rel={target === '_blank' ? 'noreferrer noopener' : rel}
+		{target}
 		{href}
 		{...restProps}
 	>
