013c7558978f3e7b161d249aaafbd104ad09f770
Falk33n <tim.falk00@gmail.com>
2024-12-05 17:54:01 +0100

    feat: create footer

---
+++ src/lib/components/sections/Footer.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { P, SocialMedia } from '$components';
+</script>
+
+<footer
+	class="flex flex-col items-center justify-center bg-secondary px-4 py-6 text-secondary-foreground"
+>
+	<nav aria-label="Footer navigation">
+		<SocialMedia />
+	</nav>
+
+	<P class="text-xs font-medium text-muted-foreground">
+		Created by Tim Falk &copy; {new Date().getFullYear()}
+	</P>
+</footer>
